ef3a0732ca2b01b8f21dc833077140772516419e
Cyrille Grandval <cgrandval@darkmira.com>
2021-05-17 23:35:22 +0200

    Add rooms

---
+++ tilesets/carpet-34x34.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="carpet-34x34" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../dmtphp20-maps/tilesets/carpet-34x34.png" width="256" height="256"/>
+</tileset>
