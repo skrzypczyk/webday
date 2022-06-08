d36cc9256052a5d039a0e8e07c1c056669119661
Yves skrzypczyk <y.skrzypczyk@gmail.com>
2022-06-08 14:47:13 +0200

    update for hackathon

---
+++ ECITV.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="ECITV" tilewidth="32" tileheight="32" tilecount="12" columns="4">
+ <image source="../IMG-20220608-WA0001.png" width="128" height="96"/>
+</tileset>
